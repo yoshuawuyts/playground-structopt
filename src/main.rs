b0348004cad17202b096c81a8f8c5c2f11a43317
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-02-07 22:30:43 +0100

    subcommands

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,66 @@ extern crate structopt_derive;
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
-#[structopt(name = "basic-example")]
-struct Opt {
-  #[structopt(short = "d", long = "debug")]
-  debug: bool,
-
-  #[structopt(short = "s", long = "speed", default_value = "42")]
-  speed: f64,
+#[structopt(name = "boats")]
+enum Cmds {
+  #[structopt(name = "add", about = "Add a thing")]
+  Add {
+    #[structopt(short = "i")]
+    interactive: bool,
+    #[structopt(short = "p")]
+    patch: bool,
+    files: Vec<String>,
+  },
+  #[structopt(name = "fetch", about = "Fetch a thing")]
+  Fetch {
+    #[structopt(long = "dry-run")]
+    dry_run: bool,
+    #[structopt(long = "all")]
+    all: bool,
+    repository: Option<String>,
+  },
+  #[structopt(name = "commit", about = "Commit a thing")]
+  Commit {
+    #[structopt(short = "m")]
+    message: Option<String>,
+    #[structopt(short = "a")]
+    all: bool,
+  },
 }
 
 fn main() {
-  let opt = Opt::from_args();
-  println!("{:?}", opt);
+  match Cmds::from_args() {
+    Cmds::Add {
+      interactive,
+      patch,
+      files,
+    } => {
+      let opts = Cmds::Add {
+        interactive,
+        patch,
+        files,
+      };
+      // this is where we'd pass the struct to some_function
+      println!("{:?}", opts);
+      1
+    }
+    Cmds::Fetch {
+      repository,
+      dry_run,
+      all,
+    } => {
+      let opts = Cmds::Fetch {
+        dry_run,
+        all,
+        repository,
+      };
+      // this is where we'd pass the struct to some_other_function
+      println!("{:?}", opts);
+      1
+    }
+    _ => {
+      println!("nop");
+      1
+    }
+  };
 }
